663bd21c8c9b977eb233a78a21fa5250684865f4
Vansh <vansh8755381346@gmail.com>
2023-01-16 10:36:54 +0530

    new

---
+++ 3.longest-substring-without-repeating-characters.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+/*
+ * @lc app=leetcode id=3 lang=java
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int temp = 0;
+        int res = 1;
+        x
+        return res;
+    }
+}
+// @lc code=end
